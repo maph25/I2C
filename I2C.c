6aa9d65d9e30b5ff29c387f622b7cd0b0def0ecf
Andrea Perez Huizar <39924790+maph25@users.noreply.github.com>
2018-11-07 13:55:36 -0600

    Corrections

--- I2C.c
+++ I2C.c
@@ -10,24 +10,19 @@
 #include "Delay.h"
 #include "I2C.h"
 
-#define MAX_COUNTER (0xFF)
-static uint8_t counter;
-uint8_t flag = FALSE;
-uint32 bus_clock;
+uint32_t baudRate;
 gpio_pin_control_register_t ctrlReg = GPIO_MUX2;
 
 void I2C_init(i2c_channel_t channel, uint32_t system_clock, uint16_t baud_rate){
 	GPIO_clock_gating (GPIO_B);
 	GPIO_pin_control_register(GPIO_B, BIT2, &ctrlReg);
 	GPIO_pin_control_register(GPIO_B, BIT3, &ctrlReg);
-	bus_clock = system_clock / 2;
+	baudRate = (system_clock)/(baud_rate*4);
 
 		switch (channel){
 		case I2C_0:{
 			SIM->SCGC4 |= SIM_SCGC4_I2C0_MASK;
 			I2C0->C1 |= I2C_C1_IICEN_MASK;
-			I2C0->F |= I2C_F_MULT(2);
-			I2C0->F |= I2C_F_ICR_MASK;
 			break;
 		}
 		case I2C_1:
@@ -87,28 +82,17 @@ uint8_t  I2C_read_byte(void){
 }
 
 void I2C_wait(void){
-	counter = 0;
-	do{
-		 counter++;
-		 if(counter >= MAX_COUNTER)
-			 flag = TRUE;
-	}while(!(I2C0->S & I2C_S_TCF_MASK));
-	counter = 0;
-	I2C0->S |= I2C_S_IICIF_MASK;
+	while(!(I2C0->S & I2C_S_TCF_MASK));
+		I2C0->S |= I2C_S_IICIF_MASK;
 }
 
 uint8_t I2C_get_ack(void){
     delay(5000);
-    counter = 0;
-    do{
-    	counter ++;
-    	if(counter >= MAX_COUNTER){
-			flag = TRUE;
-			counter = 0;
-    	}
+
+	while(I2C0->S & I2C_S_RXAK_MASK ){
     	return TRUE;
-	}while(I2C_S_RXAK_MASK & I2C0->S);
-    return FALSE;
+	}
+	return FALSE;
 }
 
 void I2C_start(void){
