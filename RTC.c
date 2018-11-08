38532b3e33eb4491d61fbaa1c74fd6fe0c394e85
Andrea Perez Huizar <39924790+maph25@users.noreply.github.com>
2018-11-08 12:07:32 -0600

    RTC: get hours

--- RTC.c
+++ RTC.c
@@ -204,3 +204,34 @@ uint8 get_minutes(){
 	/*Data return*/
 	return (data);
 }
+
+uint8 get_hours(){
+	/*Start*/
+	I2C_start();
+	/*Control byte*/
+	I2C_write_byte(RTC_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+	/*Address byte*/
+	I2C_write_byte(RTC_HOURS);
+	I2C_wait();
+	I2C_get_ack();
+	/*Restart*/
+	I2C_repeted_start();
+	/*Control byte*/
+	I2C_write_byte(RTC_SLAVE_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+	/*Change to receiver mode*/
+	I2C_tx_rx_mode(I2C_RX_MODE);
+	I2C_nack();
+	/*Dummy read*/
+	data = I2C_read_byte();
+	I2C_wait();
+	/*Stop*/
+	I2C_stop();
+	data = I2C_read_byte();
+	/*Data return*/
+	return (data);
+}
+
