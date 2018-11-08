cf3cafc3bd3d5e2d14cc098d8ab5e926a94d88e9
Andrea Perez Huizar <39924790+maph25@users.noreply.github.com>
2018-11-08 12:10:13 -0600

    RTC: get weekday

--- RTC.c
+++ RTC.c
@@ -235,3 +235,33 @@ uint8 get_hours(){
 	return (data);
 }
 
+uint8 get_weekday(){
+	/*Start*/
+	I2C_start();
+	/*Control byte*/
+	I2C_write_byte(RTC_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+	/*Address byte*/
+	I2C_write_byte(RTC_WEEKDAY);
+	I2C_wait();
+	I2C_get_ack();
+	/*Restart*/
+	I2C_repeted_start();
+	/*Control byte*/
+	I2C_write_byte(RTC_SLAVE_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+	/*Change to receiver mode*/
+	I2C_tx_rx_mode(I2C_RX_MODE);
+	I2C_nack();
+	/*Dummy read*/
+	data = I2C_read_byte();
+	I2C_wait();
+	/*Stop*/
+	I2C_stop();
+	data = I2C_read_byte();
+	/*Data return*/
+	return (data);
+}
+
