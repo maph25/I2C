da2915901f6ac7a0aaa95120fb11a45a5db09de6
Andrea Perez Huizar <39924790+maph25@users.noreply.github.com>
2018-11-14 14:14:21 -0600

    EEPROM read data

--- EEPROM.c
+++ EEPROM.c
@@ -8,10 +8,10 @@
 #include "I2C.h"
 #include "EEPROM.h"
 
-void EEPROM_write_data(uint16 address, uint8 data){
-	uint8 addressHigh;
-	uint8 addressLow;
+uint8 addressHigh;
+uint8 addressLow;
 
+void EEPROM_write_data(uint16 address, uint8 data){
 	addressHigh = (address>>EEPROM_SHIFT);
 	addressLow = (address & EEPROM_LOW_MASK);
 
@@ -38,4 +38,39 @@ void EEPROM_write_data(uint16 address, uint8 data){
 	I2C_stop();
 }
 
-uint8 EEPROM_read_data(uint16 address);
+uint8 EEPROM_read_data(uint16 address){
+	uint8 data;
+	/*Start*/
+	I2C_start();
+	I2C_tx_rx_mode(I2C_TX_MODE);
+	/*Control byte*/
+	I2C_write_byte(EEPROM_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+	/*High address byte*/
+	I2C_write_byte(addressHigh);
+	I2C_wait();
+	I2C_get_ack();
+	/*Low address byte*/
+	I2C_write_byte(addressLow);
+	I2C_wait();
+	I2C_get_ack();
+	/*Restart*/
+	I2C_repeted_start();
+	/*Control byte*/
+	I2C_write_byte(EEPROM_ADDRESS);
+	I2C_wait();
+	I2C_get_ack();
+	/*Change to receiver mode*/
+	I2C_tx_rx_mode(I2C_RX_MODE);
+	I2C_nack();
+	/*Dummy read*/
+	data = I2C_read_byte();
+	I2C_wait();
+	/*Stop*/
+	I2C_stop();
+	data = I2C_read_byte();
+	/*Data return*/
+	return (data);
+}
+
